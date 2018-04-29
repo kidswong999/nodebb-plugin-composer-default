502cb180d706fa38ebe2cae05167720b1239f107
王开智 <kidswong999@163.com>
2018-04-29 15:16:54 +0800

    Update admin.js

--- static/lib/admin.js
+++ static/lib/admin.js
@@ -1,18 +1,18 @@
 'use strict';
 /* globals $, app, socket */
 
-define('admin/plugins/composer-default', ['settings'], function(Settings) {
+define('admin/plugins/composer-stupid', ['settings'], function(Settings) {
 
 	var ACP = {};
 
 	ACP.init = function() {
-		Settings.load('composer-default', $('.composer-default-settings'));
+		Settings.load('composer-stupid', $('.composer-stupid-settings'));
 
 		$('#save').on('click', function() {
-			Settings.save('composer-default', $('.composer-default-settings'), function() {
+			Settings.save('composer-stupid', $('.composer-stupid-settings'), function() {
 				app.alert({
 					type: 'success',
-					alert_id: 'composer-default-saved',
+					alert_id: 'composer-stupid-saved',
 					title: 'Settings Saved',
 					message: 'Please reload your NodeBB to apply these settings',
 					clickfn: function() {
