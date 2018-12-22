fd270af07caa306814ebbe2f414ca0bdc29f3d2a
王开智 <kidswong999@163.com>
2018-12-22 18:49:57 +0800

    Update uploads.js

--- static/lib/composer/uploads.js
+++ static/lib/composer/uploads.js
@@ -283,7 +283,7 @@ define('composer/uploads', [
 					doneUploading = true;
 					if (uploads && uploads.length) {
 						for (var i=0; i<uploads.length; ++i) {
-							updateTextArea(filenameMapping[i], uploads[i].url);
+							updateTextArea(uploads[i].url, uploads[i].url);
 						}
 					}
 					preview.render(postContainer);
